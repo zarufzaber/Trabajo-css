c8091e16d7d8f7a10e9204ebdabb4772a6c363ac
xfibben <eyck.arturo@icloud.com>
2022-12-16 10:47:20 -0600

    timeline terminado

--- src/components/About/Time.jsx
+++ src/components/About/Time.jsx
@@ -3,89 +3,171 @@ import { Carousel } from "react-bootstrap";
 
 const Time = () => {
   return (
-    <div className={'h-96 mt-20'}>
+    <div className={"h-screen "}>
       <Carousel variant="dark">
-        <Carousel.Item>
+        <Carousel.Item className={"h-screen  -mt-20  xl:-mt-40"}>
           <img
-              className="d-block w-100 h-40 opacity-0"
+            className="d-block w-100  opacity-0"
+            src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_240,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption className={" "}>
+            <div className={'border-2 border-black rounded-2xl my-2 xl:h-80 xl:w-1/2 xl:mx-auto xl:ml-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2016
+              </h5>
+              <p className={"text-morao text-start mx-4 my-4"}>
+                ABRE LA SEDE DE GDL
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
+            <div className={'w-full h-1 bg-black my-4' }>
+
+            </div>
+            <div className={'border-2 border-black rounded-2xl  xl:h-80 xl:w-1/2 xl:mx-auto xl:mr-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2017
+              </h5>
+
+              <p className={"text-morao text-start mx-4 my-4"}>
+                SE MANEJA EL PRIMER PROTOTIPO
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+
+        <Carousel.Item className={"h-screen  -mt-20  xl:-mt-40"}>
+          <img
+              className="d-block w-100  opacity-0"
               src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_240,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg"
               alt="First slide"
           />
-          <Carousel.Caption className={'border-2 border-black '}>
-            <h5>2018</h5>
-            <p>INICIA ASOCIACIÓN CON TRI-NEX Y TRANSPORTE GENERALES</p>
-            <p>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
-              fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
-              tu contenido y cambiar la fuente
-            </p>
+          <Carousel.Caption className={" "}>
+            <div className={'border-2 border-black rounded-2xl my-2 xl:h-80 xl:w-1/2 xl:mx-auto xl:mr-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2018
+              </h5>
+              <p className={"text-morao text-start mx-4 my-4"}>
+                INICIA ASOCIACIÓN CON TRI-NEX Y TRANSPORTE GENERALES
+
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
+            <div className={'w-full h-1 bg-black my-4' }>
+
+            </div>
+            <div className={'border-2 border-black rounded-2xl  xl:h-80 xl:w-1/2 xl:mx-auto xl:ml-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2019
+              </h5>
+              <p className={"text-morao text-start mx-4 my-4"}>
+                SE AMPLÍA LA PRUEBA A ARIZONA Y NUEVO MÉXICO
+
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+
+        <Carousel.Item className={"h-screen  -mt-20  xl:-mt-40"}>
           <img
-            className="d-block w-100 h-40 opacity-0"
-            src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_240,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg"
-            alt="Second slide"
+              className="d-block w-100  opacity-0"
+              src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_240,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg"
+              alt="First slide"
           />
-          <Carousel.Caption>
-            <h5>2019</h5>
-            <p>SE AMPLÍA LA PRUEBA A ARIZONA Y NUEVO MÉXICO</p>
-            <p>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
-              fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
-              tu contenido y cambiar la fuente
-            </p>
+          <Carousel.Caption className={" "}>
+            <div className={'border-2 border-black rounded-2xl my-2 xl:h-80 xl:w-1/2 xl:mx-auto xl:ml-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2020
+              </h5>
+              <p className={"text-morao text-start mx-4 my-4"}>
+                COMIENZA LA PRODUCCIÓN!
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
+            <div className={'w-full h-1 bg-black my-4' }>
+
+            </div>
+            <div className={'border-2 border-black rounded-2xl  xl:h-80 xl:w-1/2 xl:mx-auto xl:mr-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2021
+              </h5>
+              <p className={"text-morao text-start mx-4 my-4"}>
+                SE VENDEN LOS PRIMEROS 1000 AUTOS
+
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item className={"h-screen  -mt-20  xl:-mt-40"}>
           <img
-            className="d-block opacity-0"
-            src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_240,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg"
-            alt="Third slide"
+              className="d-block w-100  opacity-0"
+              src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_240,h_336,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg"
+              alt="First slide"
           />
-          <Carousel.Caption>
-            <h5>2020</h5>
-            <p>¡COMIENZA LA PRODUCCIÓN!</p>
-            <p>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
-              fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
-              tu contenido y cambiar la fuente
-            </p>
+          <Carousel.Caption className={" "}>
+            <div className={'border-2 border-black rounded-2xl my-2 xl:h-80 xl:w-1/2 xl:mx-auto xl:mr-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2020
+              </h5>
+              <p className={"text-morao text-start mx-4 my-4"}>
+                ANALIZANDO EL MERCADO DE ENTREGAS
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
+            <div className={'w-full h-1 bg-black my-4' }>
+
+            </div>
+            <div className={'border-2 border-black rounded-2xl  xl:h-80 xl:w-1/2 xl:mx-auto xl:ml-60'}>
+              <h5 className={"text-morao text-justify text-5xl mx-4 my-4"}>
+                2022
+              </h5>
+              <p className={"text-morao text-start mx-4 my-4"}>
+                SE LANZA UNA PLATAFORMA DE LOGÍSTICA INTEGRAL DE ÚLTIMA MILLA
+
+              </p>
+              <p className={"text-justify mx-4"}>
+                Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
+                fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
+                tu contenido y cambiar la fuente
+              </p>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h5>2021</h5>
-            <p>SE VENDEN LOS PRIMEROS 1000 AUTOS</p>
-            <p>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es
-              fácil. Haz clic en "Editar texto" o doble clic aquí para agregar
-              tu contenido y cambiar la fuente
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h5>2022</h5>
-            <p>SE LANZA UNA PLATAFORMA DE LOGÍSTICA INTEGRAL DE ÚLTIMA MILLA</p>
-            <p>
-              Conocedores del mercado, empezamos a conectar operadores
-              logísticos con diferentes clientes naturales y empresas con
-              diferentes necesidades, para poder gestionar la mejor forma de
-              llevar sus encargos con nuestra red de socios logísticos.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+
       </Carousel>
     </div>
   );
