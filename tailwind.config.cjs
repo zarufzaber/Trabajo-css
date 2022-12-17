dfefff8356c6b2d1b578aaea5e15b082e7bc9bb2
zarufzaber <112907549+zarufzaber@users.noreply.github.com>
2022-12-17 07:30:07 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        ssx: ['38px', '35px'],
+        sxs: ['10px', '10px'],
+      },
       colors:{
         'morao':"#4748d8",
         'naranja':"#C45308"
