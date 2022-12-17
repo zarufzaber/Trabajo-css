d98409efb0ed010da410371ea724fc8f8144b3a7
xfibben <eyck.arturo@icloud.com>
2022-12-16 21:49:58 -0600

    footer hecho

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -3,63 +3,44 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col>
-            <Row>
-              <Col>
-                <img src="" alt="Cobox" />
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <p>Contacto</p>
-              </Col>
-              <Col>
-                <p>Tel: +51 914 327 463</p>
-                <p>Email: inbox@coboxlogistic.com</p>
-              </Col>
-            </Row>
-          </Col>
-          <Col>
-            <h1>SUSCRIBETE</h1>
-            <p>
-              Registrate y recibe noticias de Cobox. Siempre tenemos novedades
-              para nuestros clientes y partners Logisticos
-            </p>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email*</Form.Label>
-                <Form.Control type="email" placeholder="Ingresa un email" />
-                <Form.Text className="text-muted">
-                  Gracias por suscribirte
-                </Form.Text>
-              </Form.Group>
-              <Button variant="secondary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-      <footer>
-        <Container>
-          <Row>
-            <Col>
-              <span>© 2022 Creado por </span>
-              <img src="" alt="Cobox" />
-              <span> by Ronquillo</span>
-            </Col>
-            <Col>
-              <img src="" alt="Linkedin" />
-              <img src="" alt="Facebook" />
-              <img src="" alt="Twitter" />
-              <img src="" alt="Instagram" />
-            </Col>
-          </Row>
-        </Container>
-      </footer>
+    <div className={"w-full my-10 lg:flex justify-evenly lg:h-96"}>
+      <div>
+        <img className={"mx-auto"} src="src/components/Images/cobox.webp" />
+        <div className={"flex justify-evenly my-5"}>
+          <div className={""}>
+            <a href={"#"}>Contacto</a>
+          </div>
+          <div>
+            <h1 className={"mb-4"}>Tel: +51 914 327 463</h1>
+            <h1>
+              Email:<br></br> inbox@coboxlogistic.com
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className={"mx-9"}>
+        <h1 className={"my-2 text-xl "}>SUSCRÍBETE</h1>
+        <h1 className={"text-sm justify-start"}>
+          Regístrate y recibe noticias de Cobox. Siempre tenemos novedades para
+          nuestros clientes y Partners Logísticos.
+        </h1>
+        <h1 className={"my-2 text-2xl"}>Email *</h1>
+        <form className={"flex  h-10 border-2 border-black rounded-2xl"}>
+          <input
+            className={"w-2/3 hover:bg-gray-300 rounded-l-xl"}
+            type={"email"}
+            placeholder={"   Ingrese su correo"}
+          />
+          <button
+            className={
+              "bg-blue-700 w-1/3   rounded-r-xl text-white hover:bg-blue-800  "
+            }
+            type={"submit"}
+          >
+            Suscribete
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
