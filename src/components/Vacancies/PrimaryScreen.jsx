164985a93663a73d920731975366f1e6272acec4
xfibben <eyck.arturo@icloud.com>
2022-12-18 14:29:28 -0600

    fomularios terminados

--- src/components/Vacancies/PrimaryScreen.jsx
+++ src/components/Vacancies/PrimaryScreen.jsx
@@ -3,31 +3,30 @@ import { Col, Container, Row } from "react-bootstrap";
 
 const PrimaryScreen = () => {
   return (
-    <div>
-      <h1>INGENIERO ELECTRICO</h1>
-      <p>
+    <div className={" my-10 w-full font-mont"}>
+      <h1 className={"text-center text-4xl font-bold my-20 lg:text-7xl"}>
+        INGENIERO ELECTRICO
+      </h1>
+      <p className={"mx-20 font-bold  text-center lg:text-2xl lg:w-1/2 lg:mx-auto lg:my-32"}>
         Párrafo. Haz clic aquí para agregar tu propio texto y editarlo. Es
         fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu
         contenido y cambiar la fuente. En este espacio puedes contar tu historia
         y permitir que los usuarios sepan más sobre ti.
       </p>
-      <Container>
-        <Row>
-          <Col sm={2}></Col>
-          <Col>
-            <h1>UBICACION</h1>
-            <h2>Guadalajara</h2>
-          </Col>
-          <Col></Col>
-          <Col>
-            <h1>TIPO DE EMPLEO</h1>
-            <h2>Permanente</h2>
-          </Col>
-          <Col sm={2}></Col>
-        </Row>
-      </Container>
-      <hr />
-    </div>
+      <div className={"w-full font-bold grid lg:flex"}>
+        <div className={"w-1/3 mx-auto text-center my-10"}>
+          <h1>UBICACIÓN</h1>
+          <div className={"2-full h-0.5 bg-black my-2 lg:w-1/4 lg:mx-auto"}></div>
+          <h1>Guadalajara</h1>
+        </div>
+        <div className={"w-1/3 mx-auto text-center my-10"}>
+          <h1>TIPO DE EMPLEO</h1>
+          <div className={"2-full h-0.5 bg-black my-2 lg:w-1/4 lg:mx-auto"}></div>
+          <h1>Permanente</h1>
+        </div>
+      </div>
+      <hr className={'lg:my-20'} />
+    </div >
   );
 };
 
--- src/components/Vacancies/WorkMe.jsx
+++ src/components/Vacancies/WorkMe.jsx
@@ -3,75 +3,51 @@ import { Col, Container, Row } from "react-bootstrap";
 
 const WorkMe = () => {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col></Col>
-          <Col>
-            <h1>Lo que haras</h1>
-          </Col>
-          <Col></Col>
-          <Col></Col>
-          <Col></Col>
-        </Row>
-        <Row>
-          <Col></Col>
-          <Col>
-            <li>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí
-              puedes contar tu historia y permitir que tus usuarios sepan más
-              sobre ti.
-            </li>
-          </Col>
-          <Col></Col>
-          <Col>
-            <li>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí
-              puedes contar tu historia y permitir que tus usuarios sepan más
-              sobre ti.
-            </li>
-          </Col>
-          <Col></Col>
-        </Row>
-        <Row>
-          <Col></Col>
-          <Col>
-            <li>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí
-              puedes contar tu historia y permitir que tus usuarios sepan más
-              sobre ti.
-            </li>
-          </Col>
-          <Col></Col>
-          <Col>
-            <li>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí
-              puedes contar tu historia y permitir que tus usuarios sepan más
-              sobre ti.
-            </li>
-          </Col>
-          <Col></Col>
-        </Row>
-        <Row>
-          <Col></Col>
-          <Col>
-            <li>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí
-              puedes contar tu historia y permitir que tus usuarios sepan más
-              sobre ti.
-            </li>
-          </Col>
-          <Col></Col>
-          <Col>
-            <li>
-              Párrafo. Haz clic aquí para agregar tu propio texto y editar. Aquí
-              puedes contar tu historia y permitir que tus usuarios sepan más
-              sobre ti.
-            </li>
-          </Col>
-          <Col></Col>
-        </Row>
-      </Container>
+    <div className={"font-roboto my-20 w-full"}>
+      <div className={"lg:mx-48"}>
+        <h1 className={"mx-20 font-bold text-xl  font-mont"}>Lo que harás</h1>
+        <div className={"h-0.5 bg-black w-36 mx-20 my-2"}> </div>
+      </div>
+      <div className={"w-full"}>
+        <ul className={'mx-20 my-10 lg:grid lg:grid-cols-2 lg:gap-2'}>
+          <li className={'my-20 lg:mx-20 font-mont font-bold'}>
+            Párrafo. Haz clic aquí para agregar tu propio texto y editarlo. Es
+            fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu
+            contenido y cambiar la fuente. En este espacio puedes contar tu
+            historia y permitir que los usuarios sepan más sobre ti.
+          </li>
+          <li className={'my-20 lg:mx-20 font-mont font-bold'}>
+            Párrafo. Haz clic aquí para agregar tu propio texto y editarlo. Es
+            fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu
+            contenido y cambiar la fuente. En este espacio puedes contar tu
+            historia y permitir que los usuarios sepan más sobre ti.
+          </li>
+          <li className={'my-20 lg:mx-20 font-mont font-bold'}>
+            Párrafo. Haz clic aquí para agregar tu propio texto y editarlo. Es
+            fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu
+            contenido y cambiar la fuente. En este espacio puedes contar tu
+            historia y permitir que los usuarios sepan más sobre ti.
+          </li>
+          <li className={'my-20 lg:mx-20 font-mont font-bold'}>
+            Párrafo. Haz clic aquí para agregar tu propio texto y editarlo. Es
+            fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu
+            contenido y cambiar la fuente. En este espacio puedes contar tu
+            historia y permitir que los usuarios sepan más sobre ti.
+          </li>
+          <li className={'my-20 lg:mx-20 font-mont font-bold'}>
+            Párrafo. Haz clic aquí para agregar tu propio texto y editarlo. Es
+            fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu
+            contenido y cambiar la fuente. En este espacio puedes contar tu
+            historia y permitir que los usuarios sepan más sobre ti.
+          </li>
+          <li className={'my-20 lg:mx-20 font-mont font-bold'}>
+            Párrafo. Haz clic aquí para agregar tu propio texto y editarlo. Es
+            fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu
+            contenido y cambiar la fuente. En este espacio puedes contar tu
+            historia y permitir que los usuarios sepan más sobre ti.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
